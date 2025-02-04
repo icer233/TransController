23dd261b150241b8c72527096a41cb5c0e32849d
icer <74440627+icer233@users.noreply.github.com>
2025-02-04 18:45:30 +0800

    增加控件可以控制步长

--- TransController.cpp
+++ TransController.cpp
@@ -4,12 +4,13 @@
 #include <commctrl.h>
 
 // 全局变量
-HWND g_hEditBox; // 文本框句柄
-HWND g_hExitButton; // 退出按钮句柄
-HWND g_hBackgroundButton; // 后台运行按钮句柄
+HWND g_hEditBox;         // 文本框句柄
+HWND g_hExitButton;      // 退出按钮句柄
+HWND g_hBackgroundButton;// 后台运行按钮句柄
+HWND g_hStepEdit;        // 步长输入框句柄
 int currentTransparency = 100;  // 当前不透明度，默认100%
-HHOOK g_hMouseHook; // 鼠标钩子
-const wchar_t windowTitle[25] = L"透明度控制者v1.2.4 作者：ICER233";
+HHOOK g_hMouseHook;      // 鼠标钩子
+const wchar_t windowTitle[25] = L"透明度控制者v1.2.5 作者：ICER233";
 
 // 设置窗口不透明度
 void SetWindowTransparency(HWND hwnd, int transparency) {
@@ -35,10 +36,11 @@ bool RegisterHotKeys(HWND hwnd) {
     return true;
 }
 
-// 设置按钮字体为微软雅黑
-void SetButtonFont(HWND hWnd) {
+// 设置控件字体为微软雅黑
+void SetFont(HWND hWnd, int height) {
     HFONT hFont = CreateFont(
-        18, 0, 0, 0, FW_NORMAL, 0, 0, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+        height, 0, 0, 0, FW_NORMAL, 0, 0, 0, DEFAULT_CHARSET,
+        OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
         FIXED_PITCH | FF_DONTCARE, L"微软雅黑"
     );
     SendMessage(hWnd, WM_SETFONT, (WPARAM)hFont, TRUE);
@@ -56,6 +58,7 @@ void LogWindowTransparency(HWND hwnd, int transparency) {
     ss << L"已将[0x" << std::hex << (uintptr_t)hwnd << std::dec << L"][" << title << L"]的不透明度设为 " << transparency << L"%\n";
     std::wstring log = ss.str();
     SendMessage(g_hEditBox, EM_SETSEL, -1, -1);
+    SendMessage(g_hEditBox, EM_REPLACESEL, 0, 0);
     SendMessage(g_hEditBox, EM_REPLACESEL, 0, (LPARAM)log.c_str());
 }
 
@@ -64,11 +67,17 @@ void HandleMouseWheel(HWND hwnd, int delta) {
     HWND foregroundWindow = GetForegroundWindow();
     if (foregroundWindow) {
         if (GetAsyncKeyState(VK_MENU) & 0x8000 && GetAsyncKeyState(VK_SHIFT) & 0x8000) {
+            // 获取步长设置
+            wchar_t stepStr[10] = { 0 };
+            GetWindowTextW(g_hStepEdit, stepStr, 10);
+            int step = _wtoi(stepStr);
+            if (step <= 0 || step > 100) step = 2;
+
             if (delta > 0 && currentTransparency < 100) {
-                currentTransparency += 2;
+                currentTransparency += step;
             }
             else if (delta < 0 && currentTransparency > 0) {
-                currentTransparency -= 2;
+                currentTransparency -= step;
             }
             currentTransparency = max(0, min(100, currentTransparency));
             SetWindowTransparency(foregroundWindow, currentTransparency);
@@ -92,12 +101,10 @@ LRESULT CALLBACK MouseWheelProc(int nCode, WPARAM wParam, LPARAM lParam) {
     return CallNextHookEx(g_hMouseHook, nCode, wParam, lParam);
 }
 
-// 安装鼠标滚轮钩子
+// 安装 & 移除 鼠标滚轮钩子
 void InstallMouseHook() {
-    g_hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseWheelProc, NULL, 0);  // 全局钩子
+    g_hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseWheelProc, NULL, 0);
 }
-
-// 移除鼠标滚轮钩子
 void RemoveMouseHook() {
     UnhookWindowsHookEx(g_hMouseHook);
 }
@@ -106,7 +113,6 @@ void RemoveMouseHook() {
 bool IsAnotherInstanceRunning() {
     HANDLE hMutex = CreateMutex(NULL, TRUE, L"TransparencyAppMutex");
     if (GetLastError() == ERROR_ALREADY_EXISTS) {
-        // 如果互斥体已经存在，说明程序已经在运行
         CloseHandle(hMutex);
         return true;
     }
@@ -117,31 +123,41 @@ bool IsAnotherInstanceRunning() {
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
     case WM_CREATE: {
+        // 创建步长控件
+        CreateWindow(L"STATIC", L"滚轮步长：",
+            WS_CHILD | WS_VISIBLE,
+            125, 200, 70, 25, hwnd, NULL, GetModuleHandle(NULL), NULL
+        );
+
         // 创建文本框
         g_hEditBox = CreateWindowEx(
             WS_EX_CLIENTEDGE, L"EDIT", L"",
             WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_READONLY,
-            10, 10, 360, 200, hwnd, NULL, GetModuleHandle(NULL), NULL
+            10, 10, 360, 180, hwnd, NULL, GetModuleHandle(NULL), NULL
         );
 
-        // 创建后台运行按钮
+        // 创建步长设置文本框
+        g_hStepEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"2",
+            WS_CHILD | WS_VISIBLE | ES_NUMBER, 195, 200, 50, 25,
+            hwnd, (HMENU)3, GetModuleHandle(NULL), NULL);
+
+        // 创建功能按钮
         g_hBackgroundButton = CreateWindow(L"BUTTON", L"后台运行",
             WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-            65, 220, 120, 30, hwnd, (HMENU)1, GetModuleHandle(NULL), NULL);
+            65, 235, 120, 30, hwnd, (HMENU)1, GetModuleHandle(NULL), NULL);
 
         // 创建退出按钮
         g_hExitButton = CreateWindow(L"BUTTON", L"退出",
             WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-            200, 220, 120, 30, hwnd, (HMENU)2, GetModuleHandle(NULL), NULL);
+            200, 235, 120, 30, hwnd, (HMENU)2, GetModuleHandle(NULL), NULL);
 
-        // 设置字体为微软雅黑
-        HFONT hFont = CreateFont(
-            20, 0, 0, 0, FW_NORMAL, 0, 0, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
-            FIXED_PITCH | FF_DONTCARE, L"微软雅黑"
-        );
-        SendMessage(g_hEditBox, WM_SETFONT, (WPARAM)hFont, TRUE);
-        SetButtonFont(g_hBackgroundButton);
-        SetButtonFont(g_hExitButton);
+        // 统一设置字体
+        SetFont(g_hEditBox, 18);
+        SetFont(g_hStepEdit, 18);
+        SetFont(g_hBackgroundButton, 18);
+        SetFont(g_hExitButton, 18);
+        SetFont(GetDlgItem(hwnd, 3), 18); // 步长输入框
+        SetFont(GetDlgItem(hwnd, 0), 20); // 静态文本
 
         // 注册快捷键
         if (!RegisterHotKeys(hwnd)) {
@@ -149,15 +165,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             PostQuitMessage(0);
         }
 
-        // 设置窗口信息
-        HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(101)); // 需要准备一个资源ID为101的图标
+        // 设置窗口图标和标题
+        HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(101));
         SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
         SetWindowText(hwnd, windowTitle);
 
         // 禁止窗口拉伸
-        LONG style = GetWindowLong(hwnd, GWL_STYLE);
-        style &= ~WS_SIZEBOX;  // 去除拉伸边框
-        SetWindowLong(hwnd, GWL_STYLE, style);
+        SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SIZEBOX);
 
         InstallMouseHook();
         break;
@@ -172,19 +186,17 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
         break;
     }
     case WM_COMMAND: {
-        if (LOWORD(wParam) == 1) { // 后台运行按钮
+        if (LOWORD(wParam) == 1) { // 后台运行
             RunInBackground(hwnd);
         }
-        else if (LOWORD(wParam) == 2) { // 退出按钮
+        else if (LOWORD(wParam) == 2) { // 退出
             PostQuitMessage(0);
         }
         break;
     }
-    case WM_DESTROY: {// 注销快捷键和移除钩子
-        for (int i = 0; i <= 9; i++) {
-            UnregisterHotKey(hwnd, i);
-        }
-        RemoveMouseHook(); 
+    case WM_DESTROY: { // 清理资源
+        for (int i = 0; i <= 9; i++) UnregisterHotKey(hwnd, i);
+        RemoveMouseHook();
         PostQuitMessage(0);
         break;
     }
@@ -196,22 +208,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
 // 主函数
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // 检查是否有另一个实例在运行
     if (IsAnotherInstanceRunning()) {
         MessageBox(NULL, L"程序已在运行！", L"提示", MB_OK);
-        HWND hwnd = FindWindow(L"TransparencyApp", NULL);
-        if (hwnd) {
-            ShowWindow(hwnd, SW_RESTORE);  // 显示已存在的窗口
-            SetForegroundWindow(hwnd);      // 设置为前景窗口
-        }
         return 0;
     }
 
-    // 初始化Common Controls
     INITCOMMONCONTROLSEX icc = { sizeof(INITCOMMONCONTROLSEX), ICC_WIN95_CLASSES };
     InitCommonControlsEx(&icc);
 
-    // 注册窗口类
     const wchar_t* CLASS_NAME = L"TransparencyApp";
     WNDCLASS wc = {};
     wc.lpfnWndProc = WndProc;
@@ -223,19 +227,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     // 创建窗口
     HWND hwnd = CreateWindowEx(
         0, CLASS_NAME, windowTitle,
-        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 300,
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 325,
         NULL, NULL, hInstance, NULL
     );
+
     if (!hwnd) {
         MessageBox(NULL, L"无法创建窗口！", L"错误", MB_ICONERROR);
         return 1;
     }
 
-    // 显示窗口
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-    // 消息循环
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
